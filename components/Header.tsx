ac85825bd3216ed0c1a7a1196682404cb027b649
meera <meera.malhot@gmail.com>
2025-04-01 07:58:34 -0400

    header and routing

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 2vh;
+  padding: 2vh;
+  padding-top: 4vh;
+
+`;
+
+const StyledH1 = styled.h1`
+  padding: 1vh;
+`;
+
+const StyledNav = styled.nav`
+  display: flex;
+  align-items: center;
+  margin: 3vh;
+`;
+
+const StyledLink = styled(Link)`
+  padding: 2vh;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+  
+`;
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <StyledH1>Random Cat Image Generator</StyledH1>
+      <StyledNav>
+        <StyledLink href="/">Home</StyledLink>
+        <StyledLink href="/about">About</StyledLink>
+      </StyledNav>
+    </StyledHeader>
+  );
+}
