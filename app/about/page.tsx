4a2ab8b868846d91efc7b5610c9519d1319afb27
meera <meera.malhot@gmail.com>
2025-04-04 12:46:32 -0400

    .

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,9 +8,8 @@ export default function AboutPage() {
 
       <h1 style={{color: "#088F8F", textAlign:"center", padding: "2vh"}}> Welcome to the site!</h1>
       <h2 style={{color: "#088F8F", textAlign:"center" }}>This site uses an API to fetch images of cats</h2>
-      <p style={{color: "#088F8F", textAlign:"center", padding: "2vh"}}>The cats are abyssinian. According to Wikipedia, the Abyssinian is a breed of cat with a distinctive "ticked" tabby coat, in which individual hairs are banded with different colours. They are also known simply as Abys. 
-      The first members of the breed to be exhibited in England were brought there from Abyssinia 
-      (now known as Ethiopia), hence the name. Genetic studies place the breed's origins in Southeast Asia 
+      <p style={{color: "#088F8F", textAlign:"center", padding: "2vh"}}>The cats are abyssinian. According to Wikipedia, the Abyssinian is a breed of cat with a distinctive ticked tabby coat, in which individual hairs are banded with different colours. They are also known simply as Abys. 
+      The first members of the breed to be exhibited in England were brought there from Abyssinia, now known as Ethiopia, hence the name. Genetic studies place the breed's origins in Southeast Asia 
       and the coasts of the Indian Ocean, however. It is possible that the breed was introduced to Abyssinia 
       by travelers who had stopped in Calcutta.
       </p>
